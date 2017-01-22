95bdddf4597cb489ed31f5989e2dbdc60ddfd993
Lindsey Adams <linzma87@gmail.com>
2017-01-22 14:07:49 -0500

    homepage styling/positioning

--- src/js/services/search-service.js
+++ src/js/services/search-service.js
@@ -1,12 +1,15 @@
 angular.module('movieApp').service('SearchService', function($http, $location, $state) {
-    function getMovies(title) {
+
+    function searchMovies(searchTerms) {
         $http({
             method: 'GET',
-            url: "http://www.omdbapi.com/?t=" + title
+            url: "http://www.omdbapi.com/?s=" + searchTerms
         }).then(function successCallback(response) {
             console.log(response);
         }, function errorCallback(response) {
             console.log(response);
         });
     }
+
+    // searchMovies('nemo');
 });
