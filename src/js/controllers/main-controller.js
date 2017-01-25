1d4e98417c04c57e3daa4229d707e70cb0271720
Lindsey Adams <linzma87@gmail.com>
2017-01-25 15:21:08 -0500

    image added for movies with no poster available

--- src/js/controllers/main-controller.js
+++ src/js/controllers/main-controller.js
@@ -2,6 +2,12 @@ angular.module('movieApp').controller('MainController', function($state, $scope,
   this.searchTerms = '';
   this.searchMovies = function(searchTerms) {
     SearchService.searchMovies(searchTerms, function(response) {
+      let search = response.data.Search;
+      search.map(result => {
+        if(result.Poster === 'N/A') {
+          result.Poster = "http://www.newdesignfile.com/postpic/2015/02/no-icon-available_68024.png";
+        }
+      });
       SearchService.setSearchResults(response.data.Search);
       $state.current.name !== 'movieAppParent.results' ? $state.go('movieAppParent.results') : $state.reload();
     });
