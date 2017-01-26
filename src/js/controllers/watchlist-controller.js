70e09577e1c57cec8a370b63475505f7cb58346a
Lindsey Adams <linzma87@gmail.com>
2017-01-25 21:32:57 -0500

    watchlist page pulling in movies and remove button functioning

--- src/js/controllers/watchlist-controller.js
+++ src/js/controllers/watchlist-controller.js
@@ -1,3 +1,20 @@
 angular.module('movieApp').controller('WatchlistController', function($state, $scope, SearchService) {
 $('div.search').removeClass('home').addClass('results');
+
+this.populateResults = function() {
+    $scope.movieList = SearchService.getWatchlist();
+}
+
+this.getDetails = function(movie) {
+    $scope.currentMovie = movie;
+    $scope.modalOpen = true;
+}
+
+this.removeMovie = function(index) {
+  let watchlist = SearchService.getWatchlist();
+  watchlist.splice(index, 1);
+  SearchService.setWatchlist(watchlist);
+}
+
+this.populateResults();
 });
