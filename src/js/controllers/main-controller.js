e6dd7f52a13dbc16493f00a479f1f58c75c51a3e
Lindsey Adams <linzma87@gmail.com>
2017-01-26 10:57:43 -0500

    fix for more broken movie posters

--- src/js/controllers/main-controller.js
+++ src/js/controllers/main-controller.js
@@ -6,7 +6,7 @@ angular.module('movieApp').controller('MainController', function($state, $scope,
             if (response.data.Response === 'True') {
                 search = response.data.Search;
                 search.map(result => {
-                    if (result.Poster === 'N/A') {
+                    if (result.Poster === 'N/A' || result.Poster.includes('ia.media')) {
                         result.Poster = "http://www.newdesignfile.com/postpic/2015/02/no-icon-available_68024.png";
                     }
                 });
