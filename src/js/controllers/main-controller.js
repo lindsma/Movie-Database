9077bd5ffbada165df8a105ee3373ac5cfe0c70b
Lindsey Adams <linzma87@gmail.com>
2017-01-25 22:31:18 -0500

    error message added for search fail

--- src/js/controllers/main-controller.js
+++ src/js/controllers/main-controller.js
@@ -1,20 +1,27 @@
 angular.module('movieApp').controller('MainController', function($state, $scope, SearchService) {
-  this.searchTerms = '';
-  this.searchMovies = function(searchTerms) {
-    SearchService.searchMovies(searchTerms, function(response) {
-      let search = response.data.Search;
-      search.map(result => {
-        if(result.Poster === 'N/A') {
-          result.Poster = "http://www.newdesignfile.com/postpic/2015/02/no-icon-available_68024.png";
-        }
-      });
-      SearchService.setSearchResults(search);
-      $state.current.name !== 'movieAppParent.results' ? $state.go('movieAppParent.results') : $state.reload();
-    });
     this.searchTerms = '';
-  }
+    this.searchMovies = function(searchTerms) {
+        let search = '';
+        SearchService.searchMovies(searchTerms, function(response) {
+            if (response.data.Response === 'True') {
+                search = response.data.Search;
+                search.map(result => {
+                    if (result.Poster === 'N/A') {
+                        result.Poster = "http://www.newdesignfile.com/postpic/2015/02/no-icon-available_68024.png";
+                    }
+                });
+            } else {
+               search = {
+                 error: true
+               }
+            }
+            SearchService.setSearchResults(search);
+            $state.current.name !== 'movieAppParent.results' ? $state.go('movieAppParent.results') : $state.reload();
+        });
+        this.searchTerms = '';
+    }
 
-  this.revertStyles = function() {
-    $('div.search').removeClass('results').addClass('home');
-  }
+    this.revertStyles = function() {
+        $('div.search').removeClass('results').addClass('home');
+    }
 });
